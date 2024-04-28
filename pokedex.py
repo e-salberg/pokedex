f75de2e63a4a6da6b00525f4b32f86c44e6b8de1
eric <e.salberg213@gmail.com>
2024-04-27 21:39:32 -0400

    added azure text-to-speech

--- pokedex.py
+++ pokedex.py
@@ -1,12 +1,20 @@
 from pokemon import Pokemon
 import requests
 import re
+import os
+import azure.cognitiveservices.speech as speechsdk
 
 input = "charizard" #input("Enter name of pokemon: ")
 #input = "eevee"
 
 pokemon_query = "https://pokeapi.co/api/v2/pokemon/"
 pokemon_species_query = "https://pokeapi.co/api/v2/pokemon-species/"
+
+# setting up tts
+speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('AZURE_SPEECH_KEY'), region=os.environ.get('AZURE_SPEECH_REGION'))
+audio_config =  speechsdk.audio.AudioOutputConfig(use_default_speaker=True) #speechsdk.audio.PullAudioOutputStream() # speechsdk.audio.AudioConfig(use_default_speaker=True)
+speech_config.speech_synthesis_voice_name = 'en-US-AvaMultilingualNeural'
+speech_sythesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
 pokemon_res = requests.get(pokemon_query + input)
 pokemon_species_res = requests.get(pokemon_species_query + input)
@@ -23,5 +31,9 @@ description = pokemon_species_data["flavor_text_entries"].pop(0)["flavor_text"]
 description = re.subn('[\n\f]', ' ', description)[0]
 
 pokemon = Pokemon(input, pokemon_data["types"], description)
+text = pokemon.dex_entry()
 
-print(pokemon.dex_entry())
+speech_result = speech_sythesizer.speak_text_async(text).get()
+
+if speech_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized for text [{}]".format(text))
