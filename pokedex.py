5066e6caed232276d8ff7bcfeaea934dfd86c457
eric <e.salberg213@gmail.com>
2024-04-29 22:39:47 -0400

    removed dead imports

--- pokedex.py
+++ pokedex.py
@@ -1,10 +1,9 @@
 from pokemon import Pokemon
 from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
-from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
 from msrest.authentication import ApiKeyCredentials
 import azure.cognitiveservices.speech as speechsdk
-import os, time, uuid
+import os
 import requests
 import re
 
